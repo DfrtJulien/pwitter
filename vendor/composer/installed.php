4f588bd3846f62437a1ebedc1c9802e963ed6534
DfrtJulien <de.freitas.julien78@gmail.com>
2025-03-02 15:47:14 +0100

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'defre/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f2347c82e79ce3a1d2f6f4e0384623cace78fa6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'defre/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f2347c82e79ce3a1d2f6f4e0384623cace78fa6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
