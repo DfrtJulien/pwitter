a1f4deeccb403adee08c4329181a1f4d36722f13
DfrtJulien <de.freitas.julien78@gmail.com>
2025-03-10 11:59:55 +0100

    affichage des autres utilisateurs

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,14 +1,14 @@
 <?php
 require_once(__DIR__ . "/partials/head.php");
 ?>
-<div>
-  <section class="add-postes">
-    <form method="POST" id="post-form">
-      <input type="text" placeholder="Quoi de nouveau?" class="post-input" name="post">
-      <button type="submit" class="active-btn" id="submit-btn">Poster</button>
-    </form>
-  </section>
-  <section class="show-post">
+
+<section class="add-postes">
+  <form method="POST" id="post-form">
+    <input type="text" placeholder="Quoi de nouveau?" class="post-input" name="post">
+    <button type="submit" class="active-btn" id="submit-btn">Poster</button>
+  </form>
+
+  <div class="show-post">
     <div>
       <?php
       foreach ($posts as $post) {
@@ -20,7 +20,22 @@ require_once(__DIR__ . "/partials/head.php");
       }
       ?>
     </div>
-  </section>
+  </div>
+</section>
+<div>
+  <?php
+  foreach ($users as $user) {
+  ?>
+    <div>
+      <p><?= $user->getUsername() ?></p>
+      <form mthod="POST">
+        <input type="hidden" value="<?= $user->getId() ?>">
+        <button type="submit">Suivre</button>
+      </form>
+    </div>
+  <?php
+  }
+  ?>
 </div>
 <?php
 require_once(__DIR__ . "/partials/footer.php");
